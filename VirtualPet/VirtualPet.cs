8dd27a957d379f0af206e8da160df6d8d443760b
TAxford <t.axford2@yahoo.com>
2018-02-03 09:31:43 -0500

    Initial commit with fields and properties

---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //Fields of Twinkletoe the puppy
+
+        private bool isHungry;
+        private bool isTried;
+        private bool isBored;
+        private bool isDirty;
+
+
+        //Properties of Twinkletoe the puppy
+
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+
+        public bool IsBored
+        {
+            get { return this.isBored; }
+            set { this.isBored = value; }
+        }
+
+        public bool IsTired
+        {
+            get { return this.isTried; }
+            set { this.isTried = value; }
+        }
+
+
+        public bool IsDirty
+        {
+            get { return this.isDirty; }
+            set { this.isDirty = value; }
+        }
+
+        
+
+    }
+}
